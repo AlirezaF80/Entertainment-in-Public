71d0f03bdc353ad6a8b54996e12378dc9cb7cb71
AlirezaF80 <alirezafarzaneh138@gmail.com>
2024-10-11 23:37:02 +0330

    Add Tricoteuses

---
+++ Assets/Scripts/TricoteuseController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace DefaultNamespace {
+    public class TricoteuseController : MonoBehaviour {
+        [SerializeField] private Animator animator;
+        [SerializeField] private string talkCountName = "talkCount";
+        [SerializeField] private Vector2 talkCountRange;
+        [SerializeField] private Vector2 talkAnimDelayRange;
+        [SerializeField] private float talkAnimDuration;
+
+        private void Start() {
+            animator.SetInteger(talkCountName, Random.Range((int)talkCountRange.x, (int)talkCountRange.y));
+            StartCoroutine(Talk());
+        }
+        
+        private IEnumerator Talk() {
+            while (true) {
+                yield return new WaitForSeconds(talkAnimDuration);
+                animator.SetInteger(talkCountName, animator.GetInteger(talkCountName) - 1);
+                if (animator.GetInteger(talkCountName) > 0) continue;
+                yield return new WaitForSeconds(Random.Range(talkAnimDelayRange.x, talkAnimDelayRange.y));
+                animator.SetInteger(talkCountName, Random.Range((int)talkCountRange.x, (int)talkCountRange.y));
+            }
+        }
+    }
+}
