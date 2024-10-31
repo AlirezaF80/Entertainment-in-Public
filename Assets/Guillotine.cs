2862f58c6e7de8c76c747c43f1ce70c187ee507b
AlirezaF80 <alirezafarzaneh138@gmail.com>
2024-11-01 00:47:12 +0330

    Add dismantling to Guillotine

--- Assets/Guillotine.cs
+++ Assets/Guillotine.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
 
 public class Guillotine : MonoBehaviour {
+    [SerializeField] private Animator animator;
     [SerializeField] private GameObject[] obstaclePrefabs;
     [SerializeField] private float minAngle, maxAngle;
     [SerializeField] private Transform spawnPoint;
 
+    private bool isDismantled;
+
     public void SpawnObstacle() {
+        if (isDismantled) return;
         var randomRotationFactor = Random.Range(0f, 1f);
         var randomAngle = Mathf.Lerp(minAngle, maxAngle, randomRotationFactor);
         var randomRotation = Quaternion.Euler(0f, 0f, randomAngle);
         var prefabToSpawn = Random.Range(0, obstaclePrefabs.Length);
         var obstacle = Instantiate(obstaclePrefabs[prefabToSpawn], spawnPoint);
         obstacle.transform.rotation = randomRotation;
+    }
+
+    public void Dismantle() {
+        isDismantled = true;
+        animator.SetTrigger("Dismantle");
     }
 }
